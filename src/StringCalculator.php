a74ee792722ed4253500de5af2f23000a89e047e
Oihana Elizagoyen Ugalde <elizagoyen.131910@e.unavarra.es>
2022-02-27 18:30:34 +0000

    Created StringCalculator.php and StringCalcuclatorTest.php files

---
+++ src/StringCalculator.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Deg540\PHPTestingBoilerplate;
+
+class StringCalculator
+{
+
+}
---
+++ tests/StringCalculatorTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Deg540\PHPTestingBoilerplate\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class StringCalculatorTest extends TestCase
+{
+
+}
